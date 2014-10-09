b596a71dcc98e5b06be7c8828dc16f9811930307
Jeff Vettraino <jeff.vettraino@cohesiveintegrations.com>
2014-10-09 15:46:54 -0700

    ECDR-1 Initial checkin of more dynamic opensearch source.  Still needs updates

---
+++ source/cdr-opensearch-source/src/main/java/net/di2e/ecdr/source/rest/config/URLParameterHandler.java
@@ -0,0 +1,13 @@
+package net.di2e.ecdr.source.rest.config;
+
+public class URLParameterHandler {
+    
+    public void setParameterName( String key, String value ){
+        
+    }
+    
+    public String getParameterName( String key ){
+        return null;
+    }
+
+}
